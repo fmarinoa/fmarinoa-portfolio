8d7df6c4d767f614486aa1380df15beeccd2808f
fmarinoa <franco.edson.18014@gmail.com>
2025-06-15 22:08:48 -0500

    Eliminar importaciones innecesarias en el componente Footer y ajustar las rutas de las imágenes de GitHub y LinkedIn.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,4 @@
 import Image from 'next/image';
-import GitHubSvg from '../../public/github-svgrepo-com.svg';
-import LinkedInSvg from '../../public/linkedin-svgrepo-com.svg'
 
 export default function Footer() {
   const year = new Date().getFullYear();
@@ -10,9 +8,8 @@ export default function Footer() {
       <div className="max-w-4xl mx-auto px-4 flex flex-col md:flex-row items-center md:justify-between gap-4">
         {/* Texto alineado a la izquierda */}
         <span className="w-full md:w-auto text-left text-sm text-gray-400 md:order-1 order-2">
-            &copy; {year} Franco Mariño.
-            <br />
-            Todos los derechos reservados.
+          &copy; {year} Franco Mariño.
+          <br /> Todos los derechos reservados.
         </span>
         {/* Íconos alineados a la derecha */}
         <div className="flex gap-4 md:order-2 order-1">
@@ -23,7 +20,7 @@ export default function Footer() {
             aria-label="GitHub"
             className="hover:text-gray-400 text-2xl"
           >
-            <Image src={GitHubSvg} alt="GitHub" width={32} height={32} />
+            <Image src="/github-svgrepo-com.svg" alt="GitHub" width={32} height={32} />
           </a>
           <a
             href="https://www.linkedin.com/in/fmarinoa/"
@@ -32,7 +29,7 @@ export default function Footer() {
             aria-label="LinkedIn"
             className="hover:text-gray-400 text-2xl"
           >
-            <Image src={LinkedInSvg} alt="LinkedIn" width={32} height={32} />
+            <Image src="/linkedin-svgrepo-com.svg" alt="LinkedIn" width={32} height={32} />
           </a>
         </div>
       </div>
