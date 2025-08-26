4d03356c13ccb4484a746a05a6cc88bb90fe1b12
fmarinoa <franco.edson.18014@gmail.com>
2025-08-25 21:21:19 -0500

    feat(analytics): add @vercel/analytics dependency and inject in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import './styles/index.css';
 
+import { inject } from '@vercel/analytics';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
