95f58af4212a4ef28f7b9059053f08cb79faceb0
fmarinoa <franco.edson.18014@gmail.com>
2025-09-21 16:16:28 -0500

    fix(careers): update logo variable names for clarity

--- src/data/careers.ts
+++ src/data/careers.ts
@@ -1,10 +1,10 @@
-import idat from '@/assets/education/idat.webp'
-import upc from '@/assets/education/upc.webp'
+import idatLogo from '@/assets/education/idat.webp'
+import upcLogo from '@/assets/education/upc.webp'
 import type { Career } from '@/types'
 
 import { careersData } from './careersData'
 
 export const careers: Career[] = [
-  { ...careersData[0], logo: upc },
-  { ...careersData[1], logo: idat },
+  { ...careersData[0], logo: upcLogo },
+  { ...careersData[1], logo: idatLogo },
 ]
