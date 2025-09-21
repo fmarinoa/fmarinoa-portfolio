a0761a8edd2eac8300f8d04ecc303d0efe29f03a
fmarinoa <franco.edson.18014@gmail.com>
2025-09-20 23:29:33 -0500

    fix: actualizar el comando del servidor web en la configuración de Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run preview',
     url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
   },
