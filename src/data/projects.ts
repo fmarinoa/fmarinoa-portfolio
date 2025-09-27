fb57fdf1a604dbb063d761adc1fbff8bafb859eb
fmarinoa <franco.edson.18014@gmail.com>
2025-09-27 10:26:56 -0500

    fix(projects): update technologies for personal portfolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,3 @@
-import franco from '@/assets/photos/franco.webp'
 import scrapper from '@/assets/projects/scrapper.webp'
 import whatismyip from '@/assets/projects/whatismyip.webp'
 import type { Project } from '@/types'
@@ -7,5 +6,5 @@ import { projectsData } from './projectsData'
 export const projects: Project[] = [
   { ...projectsData[0], imageUrl: scrapper },
   { ...projectsData[1], imageUrl: whatismyip },
-  { ...projectsData[2], imageUrl: franco },
+  { ...projectsData[2] },
 ]
