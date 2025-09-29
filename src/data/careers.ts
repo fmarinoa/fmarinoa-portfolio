8c18e2b6b1e58d2e69b3a6fdca0e5fb7b9d99864
fmarinoa <franco.edson.18014@gmail.com>
2025-09-29 00:49:05 -0500

    fix(data): apply various code improvements and cleanups

--- src/data/careers.ts
+++
@@ -1,9 +0,0 @@
-import idatLogo from '@/assets/education/idat.webp'
-import upcLogo from '@/assets/education/upc.webp'
-import type { Career } from '@/types'
-import { careersData } from './careersData'
-
-export const careers: Career[] = [
-  { ...careersData[0], logo: upcLogo },
-  { ...careersData[1], logo: idatLogo },
-]
