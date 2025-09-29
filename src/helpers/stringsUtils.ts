fe722ec3b77c6412d6852efc083ecfbae43da871
fmarinoa <franco.edson.18014@gmail.com>
2025-09-28 22:20:34 -0500

    feat(experience): format job periods for display

--- src/helpers/stringsUtils.ts
+++ src/helpers/stringsUtils.ts
@@ -3,3 +3,17 @@ export const capitalize = (str: string): string =>
 
 export const toKebabOrLower = (str: string): string =>
   str.toLowerCase().replace(/\s+/g, '-')
+
+export function formatPeriod(period: { start: string; end?: string }): string {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+  } as const
+  const start = new Date(period.start).toLocaleDateString('es-PE', options)
+  const end =
+    period.end === 'present' || !period.end
+      ? capitalize(new Date().toLocaleDateString('es-PE', options))
+      : new Date(period.end).toLocaleDateString('es-PE', options)
+
+  return `${capitalize(start)} - ${capitalize(end)}`
+}
