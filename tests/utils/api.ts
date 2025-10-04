8a162e1c5591b54aeaec652c8227f06707d5762d
fmarinoa <franco.edson.18014@gmail.com>
2025-10-03 21:08:00 -0500

    refactor(api): use CONTENT_BASE_URL consistently

--- tests/utils/api.ts
+++ tests/utils/api.ts
@@ -1,44 +1,46 @@
+const { CONTENT_BASE_URL } = process.env
+
 export async function fetchCVUrl(): Promise<string> {
-  return fetch(process.env.CONTENT_BASE_URL + '/data/urls.json')
+  return fetch(CONTENT_BASE_URL + '/data/urls.json')
     .then(response => response.json())
     .then(data => data.cv)
     .catch(() => '')
 }
 
 export async function fetchCareers(): Promise<any[]> {
-  return fetch(process.env.CONTENT_BASE_URL + '/data/careers.json')
+  return fetch(CONTENT_BASE_URL + '/data/careers.json')
     .then(response => response.json())
     .catch(() => [])
 }
 
 export async function fetchCourses(): Promise<any[]> {
-  return fetch(process.env.CONTENT_BASE_URL + '/data/courses.json')
+  return fetch(CONTENT_BASE_URL + '/data/courses.json')
     .then(response => response.json())
     .catch(() => [])
 }
 
 export async function fetchFooterSocials(): Promise<any> {
-  return fetch(process.env.CONTENT_BASE_URL + '/data/footerInfo.json')
+  return fetch(CONTENT_BASE_URL + '/data/footerInfo.json')
     .then(response => response.json())
     .then(data => data.socials)
     .catch(() => ({}))
 }
 
 export async function fetchFooterLinks(): Promise<any> {
-  return fetch(process.env.CONTENT_BASE_URL + '/data/footerInfo.json')
+  return fetch(CONTENT_BASE_URL + '/data/footerInfo.json')
     .then(response => response.json())
     .then(data => data.links)
     .catch(() => ({}))
 }
 
 export async function fetchJobs(): Promise<any[]> {
-  return fetch(process.env.CONTENT_BASE_URL + '/data/jobs.json')
+  return fetch(CONTENT_BASE_URL + '/data/jobs.json')
     .then(response => response.json())
     .catch(() => [])
 }
 
 export async function fetchProjects(): Promise<any[]> {
-  return fetch(process.env.CONTENT_BASE_URL + '/data/projects.json')
+  return fetch(CONTENT_BASE_URL + '/data/projects.json')
     .then(response => response.json())
     .catch(() => [])
 }
